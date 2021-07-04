4a5f6c4ae2e885b20017c31dd43cc3741fcbb8e6
Linda Okorie <lindaokorie27@gmail.com>
2021-07-04 17:56:48 +0100

    created basic structure for page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import Popup from './components/Popup';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <button className="btn primary cta-btn">Unfollow</button>
 
-export default App;
+        <Popup />
+      </div>
+    )
+  }
+}
+export default App
