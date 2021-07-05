3d49f61095a906a8664afc87f35519b64dfc99f1
Linda Okorie <lindaokorie27@gmail.com>
2021-07-05 20:01:13 +0100

    restored previous changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Popup from './components/Popup';
+import Button from './components/Button'
 import './App.css';
 
 class App extends Component {
@@ -9,16 +10,43 @@ class App extends Component {
     this.state = {
        showModal: false
     }
+
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleCancelAction = this.handleCancelAction.bind(this);
+    this.handleUnfollowAction = this.handleUnfollowAction.bind(this);
   }
   
+  handleButtonClick = (event) => {
+    if(event.target.innerText === "Unfollow"){
+      this.setState({
+        showModal: true
+      })
+    }
+  }
+
+  handleUnfollowAction = () => {
+    let btn = document.getElementById("btn1");
+    btn.innerText = "Follow";
+
+    this.setState({
+      showModal: false
+    })
+  }
+
+  handleCancelAction = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+
   render() {
 
     const showModal = this.state.showModal;
     return (
       <div className="App">
-        <button className="btn primary cta-btn">Unfollow</button>
+        <Button onClick={this.handleButtonClick}/>
 
-        {showModal ? <Popup memberTag="@superfunnymemes"/> : null}
+        {showModal ? <Popup memberTag="@superfunnymemes" onUnfollow={this.handleUnfollowAction} onCancel={this.handleCancelAction} /> : null}
       </div>
     )
   }
