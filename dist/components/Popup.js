2a6d17a1b5cda4fcf4152986633928af69768684
Linda Okorie <lindaokorie27@gmail.com>
2021-07-06 11:45:38 +0100

    rebuilt production files

--- dist/components/Popup.js
+++ dist/components/Popup.js
@@ -9,6 +9,8 @@ var _react = _interopRequireDefault(require("react"));
 
 var _reactDom = _interopRequireDefault(require("react-dom"));
 
+require("../styles/Popup.css");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const Popup = props => {
